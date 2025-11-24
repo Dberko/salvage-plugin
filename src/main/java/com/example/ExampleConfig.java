5a470de21c83e80608e2e78c6ab2b8a8074939b5
berko <doabarrellroll7@gmail.com>
2025-11-24 16:16:49 -0500

    fix configgroup

--- src/main/java/com/example/ExampleConfig.java
+++ src/main/java/com/example/ExampleConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("Salvage")
 public interface ExampleConfig extends Config
 {
 }
